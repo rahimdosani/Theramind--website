35d2f4b2543fdf70b4502e5817084c6d05f5636c
rahimdosani <rahimdosani21@gmail.com>
2025-10-02 20:58:39 +0530

    Fix: added Procfile and Render port config

--- app.py
+++ app.py
@@ -448,3 +448,7 @@ def ebooks():
 if __name__ == '__main__':
     setup_databases()
     app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
